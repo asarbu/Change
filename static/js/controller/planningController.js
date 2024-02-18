57fb8e58932b8f2c4622c268d6218efdbd1603ed
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2024-02-18 12:50:41 +0200

    Simplified date getters

--- static/js/controller/planningController.js
+++ static/js/controller/planningController.js
@@ -16,7 +16,7 @@ export default class PlanningController {
 	}
 
 	async init(forYear) {
-		const year = forYear || new Date().toLocaleString('en-US', { year: 'numeric' });
+		const year = forYear || new Date().getFullYear();
 		this.#caches = await PlanningCache.getAll();
 
 		let defaultYearCache;
