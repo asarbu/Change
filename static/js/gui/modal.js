f44110cb8470ed172dd4e872826474a54b71ff0d
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2024-03-04 15:55:34 +0200

    Improved modal for month dropup

--- static/js/gui/modal.js
+++ static/js/gui/modal.js
@@ -1,12 +1,13 @@
-import Dom from "./dom"
+import Dom from "./dom.js"
 
 export default class Modal {
 	constructor(id) {
-		this.modal = new Dom('div').id(id).cls('modal');
+		this.id = id;
+		this.modal = new Dom('div').id(id).cls('modal').onClick(this.close.bind(this));
 		this.modalHtml = this.modal.toHtml();
 
 		this.content = new Dom('div').cls('modal-content');
-		this.contentHtml = this.modal.toHtml();
+		this.contentHtml = this.content.toHtml();
 		this.modalHtml.appendChild(this.contentHtml);
 	}
 
@@ -37,13 +38,35 @@ export default class Modal {
 		return this;
 	}
 
+	addCancelFooter() {
+		this.cancelButton = new Dom('h3').id(`modal-cancel-${this.id}`).text('Cancel');
+		this.cancelButtonHtml = this.cancelButton.toHtml();
+
+		this.footer(
+			this.cancelButton.onClick(this.close.bind(this)),
+		);
+
+		return this;
+	}
+
+	toDom() {
+		return this.modal;
+	}
+
+	toHtml() {
+		return this.modalHtml;
+	}
+
 	open() {
 		this.modalHtml.classList.add('show-modal-backdrop');
 		this.contentHtml.classList.add('show-modal-content');
 	}
 
-	close() {
-		this.modalHtml.classList.remove('show-modal-backdrop');
-		this.contentHtml.classList.remove('show-modal-content');
+	close(event) {
+		// Force close if it function not triggered by an event (triggered by code)
+		if (!event || event.target === this.cancelButtonHtml || event.target === this.modalHtml) {
+			this.modalHtml.classList.remove('show-modal-backdrop');
+			this.contentHtml.classList.remove('show-modal-content');
+		}
 	}
 }
