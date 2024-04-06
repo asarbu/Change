3915c82e159225fccabdaddea1f1bca1ddd0ffd2
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2024-04-06 11:45:38 +0300

    Enhanced JS build scripts to avoid cmdline operations

--- dist/js/app.js
+++
@@ -1,5 +0,0 @@
-/*if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .catch(err => console.log("service worker not registered", err))
-}*/
--- node_scripts/build.js
+++ node_scripts/build.js
@@ -1,6 +1,59 @@
 const fs = require('fs');
+const uglify = require('uglify-js');
+const rollup = require('rollup');
 
+if (!fs.existsSync('./dist')) {
+	fs.mkdirSync('./dist');
+}
+
+if (fs.existsSync('./dist/planning.json')) {
+	fs.unlink('./dist/planning.json', (error) => { if (error) throw new Error(error); });
+}
 fs.copyFile('./static/js/planning.json', './dist/planning.json', () => {});
+
+if (fs.existsSync('./dist/manifest.json')) {
+	fs.unlink('./dist/manifest.json', (error) => { if (error) throw new Error(error); });
+}
 fs.copyFile('./manifest.json', './dist/manifest.json', () => {});
+
+if (fs.existsSync('./dist/style.css')) {
+	fs.unlink('./dist/style.css', (error) => { if (error) throw new Error(error); });
+}
 fs.copyFile('./static/css/style.css', './dist/style.css', () => {});
-fs.copyFile('./static/js/planning.json', './dist/planning.json', () => {});
+
+if (fs.existsSync('./dist/index.html')) {
+	fs.unlink('./dist/index.html', (error) => { if (error) throw new Error(error); });
+}
+fs.copyFile('./static/html/index.html', './dist/index.html', () => {});
+
+if (fs.existsSync('./dist/app.js')) {
+	fs.unlink('./dist/app.js', (error) => { if (error) throw new Error(error); });
+}
+
+const inputOptions = {
+	input: './static/js/app.js',
+};
+
+const outputOptions = {
+	format: 'es',
+	file: './app.js',
+};
+
+rollup.rollup(inputOptions).then((bundle) => {
+	bundle.generate(outputOptions).then(({ output }) => {
+		output.forEach((chunk) => {
+			const uglified = uglify.minify(chunk.code, { compress: true, mangle: true });
+			if (uglified.error) {
+				throw uglified.error;
+			}
+			if (!fs.existsSync('./dist/js')) {
+				fs.mkdirSync('./dist/js');
+			}
+			fs.writeFile(`./dist/js/${chunk.fileName}`, uglified.code, (error) => {
+				if (error) {
+					throw error;
+				}
+			});
+		});
+	});
+});
