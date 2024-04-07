8ff81602628145e0f59a0cc569a2042c39a9b386
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2024-04-07 10:26:49 +0300

    Implemented year redirects for spending

---
+++ static/js/utils/utils.js
@@ -0,0 +1,23 @@
+export default class Utils {
+	// TODO replace this with date constructs to parse months?
+	static #MONTH_NAMES = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	/**
+	 * Converts to integers short month names (Jan, Feb, etc.)
+	 * @param {string} name Name of the month to parse
+	 * @returns {number}
+	 */
+	static monthForName(name) {
+		if (!Utils.#MONTH_NAMES.includes(name)) return undefined;
+		return Utils.#MONTH_NAMES.indexOf(name);
+	}
+
+	/**
+	 * Converts month integers to string names (Jan, Feb, etc.)
+	 * @param {number} month Month number to parse
+	 * @returns {string}
+	 */
+	static nameForMonth(month) {
+		return Utils.#MONTH_NAMES.at(month);
+	}
+}
