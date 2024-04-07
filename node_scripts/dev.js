a4cc7fcedfa9f3a626956d44d8c3ef5328e93dff
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2024-04-07 10:24:59 +0300

    Fixed icon paths

--- node_scripts/dev.js
+++ node_scripts/dev.js
@@ -6,6 +6,7 @@ if (fs.existsSync('./dist')) {
 	fs.mkdirSync('./dist');
 }
 
+// Use symlinks to avoid needing to reload changed files during development
 const distJsPath = path.resolve('./dist/js');
 const staticJsPath = path.resolve('./static/js');
 fs.symlink(staticJsPath, distJsPath, 'junction', (error) => { if (error) throw new Error(error); });
@@ -14,7 +15,12 @@ const distCssPath = path.resolve('./dist/css');
 const staticCssPath = path.resolve('./static/css');
 fs.symlink(staticCssPath, distCssPath, 'junction', (error) => { if (error) throw new Error(error); });
 
+const distIconsPath = path.resolve('./dist/icons');
+const staticIconsPath = path.resolve('./static/icons');
+fs.symlink(staticIconsPath, distIconsPath, 'junction', (error) => { if (error) throw new Error(error); });
+
 fs.copyFile('./static/js/planning.json', './dist/planning.json', () => {});
 fs.copyFile('./manifest.json', './dist/manifest.json', () => {});
 fs.copyFile('./static/html/index.html', './dist/index.html', (error) => { if (error) throw new Error(error); });
 fs.copyFile('./static/html/planning.html', './dist/planning.html', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/icons/favicon.ico', './dist/favicon.ico', (error) => { if (error) throw new Error(error); });
