3f7fc9074ec2033495d2a9c21275b94a33607f44
Sebastian <alexandrusebastian.sarbu@gmail.com>
2024-04-18 19:34:56 +0300

    Added spending views deleted by mistake

---
+++ static/js/spending/view/spendingNavbarHandlers.js
@@ -0,0 +1,13 @@
+export default class SpendingNavbarEventHandlers {
+	onMonthChanged = undefined;
+
+	onYearChanged = undefined;
+
+	onClickEdit = undefined;
+
+	onClickSave = undefined;
+
+	onClickAddSpending = undefined;
+
+	onClickSummary = undefined;
+}
