7088e4764b55f8e4bd278aa8013f1cc7ff172162
asarbu <sarbu_alexandru_92@yahoo.com>
2025-04-30 15:27:55 +0300

    Fix linter code quality issues

--- static/sw.js
+++ static/sw.js
@@ -1,68 +1,67 @@
-const staticCacheName = "change-app-static-v1"
-const dynamicCacheName = "change-app-dynamic-v1" 
+const staticCacheName = 'change-app-static-v1';
+const dynamicCacheName = 'change-app-dynamic-v1';
 const assets = [
-	"static/css/materialize.css",
-	"static/js/app.js",
-	"static/js/gDrive.js",
-	"static/js/gui.js",
-	"static/js/icons.js",
-	"static/js/idb.js",
-	"static/js/init.js",
-	"static/js/materialize.js",
-	"static/js/planningCache.js",
-	"static/js/planningController.js",
-	"static/js/planningGdrive.js",
-	"static/js/planningTab.js",
-	"static/js/settings.js",
-	"static/js/spendingCache.js",
-	"static/js/spendingController.js",
-	"static/js/spendingGdrive.js",
-	"static/js/spendingTab.js",
-	"static/manifest.json",
+	'static/css/materialize.css',
+	'static/js/app.js',
+	'static/js/gDrive.js',
+	'static/js/gui.js',
+	'static/js/icons.js',
+	'static/js/idb.js',
+	'static/js/init.js',
+	'static/js/materialize.js',
+	'static/js/planningCache.js',
+	'static/js/planningController.js',
+	'static/js/planningGdrive.js',
+	'static/js/planningTab.js',
+	'static/js/settings.js',
+	'static/js/spendingCache.js',
+	'static/js/spendingController.js',
+	'static/js/spendingGdrive.js',
+	'static/js/spendingTab.js',
+	'static/manifest.json',
 
-	"static/icons/icon-144x144.png",
-	"static/icons/menu.svg",
-]
+	'static/icons/icon-144x144.png',
+	'static/icons/menu.svg',
+];
 
-self.addEventListener("install", installEvent => {
-	installEvent.waitUntil(caches.open(staticCacheName).then(cache => {
-		console.log("Installing service worker: Fetching number of files:", assets.length);
+window.addEventListener('install', (installEvent) => {
+	installEvent.waitUntil(caches.open(staticCacheName).then((cache) => {
 		try {
 			cache.addAll(assets);
-			console.log('ServiceWorker installed');
 		} catch (err) {
-			for (let i of assets) {
+			const errors = [];
+			assets.forEach((i) => {
 				try {
 					cache.add(i);
-				} catch (err) {
-					console.warn("Error at fetching file:", i);
+				} catch (e) {
+					errors.push(`Error at fetching file: ${i}. ${e.message}`);
 				}
-			}
-			console.log('ServiceWorker not installed');
+			});
+			throw new Error(`Error at fetching files: ${errors.join(', ')}`);
 		}
 	}));
+});
 
-})
+window.addEventListener('activate',	(activateEvent) => activateEvent
+	.waitUntil(
+		caches.keys().then(
+			(keys) => Promise.all(
+				keys.filter((key) => key !== staticCacheName)
+					.map((key) => caches.delete(key)),
+			),
+		),
+	));
 
-self.addEventListener("activate", activateEvent => {
-    activateEvent.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName)
-                .map(key => caches.delete(key))
-                )
-		})
-    )
-})
-
-self.addEventListener("fetch", fetchEvent => {
-	fetchEvent.respondWith(
-	caches.match(fetchEvent.request).then(res => {
-		return res || fetch(fetchEvent.request); /*.then(fetchRes => {
-			return caches.open(dynamicCacheName).then(cache => {
-				cache.put(fetchEvent.request.url, fetchRes.clone());
-				return fetchRes; })
-			})*/
-		})
-	)
-})
+window.addEventListener('fetch', (fetchEvent) => fetchEvent.respondWith(
+	caches
+		.match(fetchEvent.request)
+		.then((res) => res
+			|| fetch(fetchEvent.request)
+			// TODO use only real cache after V1.0
+			|| fetch(fetchEvent.request).then(
+				(fetchRes) => caches.open(dynamicCacheName).then((cache) => {
+					cache.put(fetchEvent.request.url, fetchRes.clone());
+					return fetchRes;
+				}),
+			)),
+));
