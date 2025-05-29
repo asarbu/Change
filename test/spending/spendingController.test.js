1edd796a0d9d566592112344bfadad049d6d6881
asarbu <uidj5379@contiwan.com>
2025-05-29 15:05:39 +0300

    Rework tests

--- test/spending/spendingController.test.js
+++ test/spending/spendingController.test.js
@@ -10,17 +10,26 @@ import SpendingCache from '../../static/js/spending/persistence/spendingCache';
 import Spending from '../../static/js/spending/model/spending';
 import Planning, { Statement } from '../../static/js/planning/model/planningModel';
 import SpendingReport from '../../static/js/spending/model/spendingReport';
+import Settings from '../../static/js/settings/model/settings';
+import GDriveSettings from '../../static/js/settings/model/gDriveSettings';
 
 describe('Spending controller', () => {
-	/** @type {SpendingController} */
-	let spendingController;
-
 	/**
 	 * @param {Date} forDate
 	 * @returns {Planning}
 	 */
 	function defaultPlanning(forDate) {
 		return new Planning(forDate.getTime(), forDate.getFullYear, forDate.getMonth, []);
+	}
+
+	async function init(dateTimeProvider, settings) {
+		const currentSettings = settings ?? new Settings(new GDriveSettings(false, false));
+
+		const controller = new SpendingController(dateTimeProvider, currentSettings);
+		controller.init();
+		const cache = SpendingCache.for(dateTimeProvider.getFullYear());
+
+		return { controller, cache };
 	}
 
 	beforeAll(async () => {
@@ -32,20 +41,19 @@ describe('Spending controller', () => {
 		const main = document.createElement('main');
 		main.id = 'main';
 		document.body.appendChild(main);
-
-		const now = new Date('1800-01-13');
-		spendingController = new SpendingController(now.getFullYear(), now.getMonth(), '');
-		await spendingController.init();
 	});
 
 	it('is defined for current year', () => {
-		expect(spendingController).toBeDefined();
+		const now = new Date(1800, 0);
+		const { controller } = init(now);
+		expect(controller).toBeDefined();
 	});
 
 	it('persists spending to cache', async () => {
 		const now = new Date('1800-01-12');
 		const newSpending = new Spending(1, Statement.EXPENSE, now, 'Category', 'Descritpion', 9.99);
-		await spendingController.onCreatedSpending(newSpending);
+		const { controller } = init(now);
+		await controller.onCreatedSpending(newSpending);
 		const spendingCache = new SpendingCache(now.getFullYear());
 		const cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(1);
@@ -54,8 +62,9 @@ describe('Spending controller', () => {
 
 	it('updates spending in cache', async () => {
 		const now = new Date('1800-01-12');
+		const { controller, cache } = init(now);
 		const spending = new Spending(1, Statement.EXPENSE, now, 'Category', 'Descritpion', 9.99);
-		await spendingController.onCreatedSpending(spending);
+		await controller.onCreatedSpending(spending);
 		spending.description = 'Updated category';
 		const spendingReport = new SpendingReport(
 			now.getFullYear(),
@@ -63,9 +72,7 @@ describe('Spending controller', () => {
 			defaultPlanning(now),
 		);
 		spendingReport.appendSpending(spending);
-		await spendingController.onSavedReport(spendingReport);
-		const spendingCache = new SpendingCache(now.getFullYear());
-		const cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
+		const cachedSpendings = await cache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(1);
 		expect(cachedSpendings[0]).toEqual(spending);
 	});
@@ -74,12 +81,11 @@ describe('Spending controller', () => {
 		const now = new Date('1800-01-12');
 		const spending1 = new Spending(1, Statement.EXPENSE, now, 'Category', 'Descritpion', 9.99);
 		const spending2 = new Spending(2, Statement.EXPENSE, now, 'Category 2', 'Descritpion 2', 9.99);
-		const spendingCache = SpendingCache.for(now.getFullYear());
+		const { controller, cache } = await init(now);
 
-		await spendingController.init();
-		await spendingController.onCreatedSpending(spending1);
-		await spendingController.onCreatedSpending(spending2);
-		let cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
+		await controller.onCreatedSpending(spending1);
+		await controller.onCreatedSpending(spending2);
+		let cachedSpendings = await cache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(2);
 
 		const spendingReport = new SpendingReport(
@@ -88,8 +94,8 @@ describe('Spending controller', () => {
 			defaultPlanning(now),
 		);
 		spendingReport.appendSpending(spending1);
-		await spendingController.onSavedReport(spendingReport);
-		cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
+		await controller.onSavedReport(spendingReport);
+		cachedSpendings = await cache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(1);
 		expect(cachedSpendings[0]).toEqual(spending1);
 	});
@@ -100,8 +106,10 @@ describe('Spending controller', () => {
 		const spending2 = new Spending(2, Statement.EXPENSE, now, 'Category 2', 'Descritpion 2', 9.99);
 		const spendingCache = SpendingCache.for(now.getFullYear());
 
-		await spendingController.onCreatedSpending(spending1);
-		await spendingController.onCreatedSpending(spending2);
+		const { controller } = await init(now);
+		controller.init();
+		await controller.onCreatedSpending(spending1);
+		await controller.onCreatedSpending(spending2);
 		let cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(2);
 
@@ -112,7 +120,7 @@ describe('Spending controller', () => {
 		);
 		spendingReport.appendSpending(spending1);
 		spendingReport.appendSpending(spending2);
-		await spendingController.onDeletedReport(spendingReport);
+		await controller.onDeletedReport(spendingReport);
 		cachedSpendings = await spendingCache.readAllForMonth(now.getMonth());
 		expect(cachedSpendings.length).toBe(0);
 	});
