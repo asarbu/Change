99579dfb51f4571bab97b033aa3bfe2ee6c443b7
Alexandru-Sebastian SARBU <uidj5379@contiwan.com>
2025-07-11 14:25:21 +0300

    Add compress options to build

---
+++ node_scripts/compress.cjs
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const uglify = require('uglify-js');
+const rollup = require('rollup');
+
+if (fs.existsSync('./dist')) {
+	fs.rmSync('./dist', { recursive: true, force: true }, (error) => { if (error) throw new Error(error); });
+}
+fs.mkdirSync('./dist');
+fs.mkdirSync('./dist/js');
+fs.mkdirSync('./dist/css');
+
+fs.copyFile('./static/js/planning.json', './dist/planning.json', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./manifest.json', './dist/manifest.json', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/css/style.css', './dist/css/style.css', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/html/index.html', './dist/index.html', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/html/planning.html', './dist/planning.html', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/html/settings.html', './dist/settings.html', (error) => { if (error) throw new Error(error); });
+fs.copyFile('./static/icons/favicon.ico', './dist/favicon.ico', (error) => { if (error) throw new Error(error); });
+fs.cp('./static/icons', './dist/icons', { recursive: true }, (error) => { if (error) throw new Error(error); });
+
+const inputOptions = {
+	input: './static/js/app.js',
+	treeshake: true,
+};
+
+const outputOptions = {
+	format: 'es',
+	file: './app.js',
+};
+
+rollup.rollup(inputOptions).then((bundle) => {
+	bundle.generate(outputOptions).then(({ output }) => {
+		output.forEach((chunk) => {
+			const uglified = uglify.minify(chunk.code, {
+				compress: {
+					evaluate: 'eager',
+					hoist_funs: true,
+					module: true,
+					toplevel: true,
+					reduce_vars: true,
+					collapse_vars: true,
+				},
+				mangle: {
+					toplevel: true,
+					properties: {
+						regex: /.*/,
+					},
+				},
+				webkit: true,
+				module: true,
+				toplevel: true,
+				keep_fnames: false,
+				warnings: 'verbose',
+			});
+			if (uglified.error) { throw uglified.error;	}
+
+			if (!fs.existsSync('./dist/js')) {
+				fs.mkdirSync('./dist/js');
+			}
+
+			fs.writeFile(`./dist/js/${chunk.fileName}`, uglified.code, (error) => {
+				if (error) { throw error; }
+			});
+		});
+	});
+});
