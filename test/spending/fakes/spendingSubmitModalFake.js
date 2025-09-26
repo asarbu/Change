a5aea26accb1750a37409f5d67f152764133a0de
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-26 12:23:44 +0300

    Add tests for spending submit modal

--- test/spending/fakes/spendingSubmitModalFake.js
+++ test/spending/fakes/spendingSubmitModalFake.js
@@ -14,4 +14,36 @@ export default class SpendingSubmitModalFake  extends SpendingSubmitModal {
         }
         return null;
     }
+
+    	/**
+	 * 
+	 * @param {string} name 
+	 * @param {string} value 
+	 */
+	updateInputValue(name, value) {
+		const input = this.toHtml().querySelector(`#${name}`);
+		input.focus();
+		input.value = ''; // Clear the input before typing
+		for (let i = 0; i < value.length; i++) {
+			input.value += value.charAt(i); // Update the input value
+			input.dispatchEvent(new KeyboardEvent('keyup', { key: value.charAt(i) }));
+			input.dispatchEvent(new Event('input', { bubbles: true }));
+		}
+		return this;
+	}
+
+    setInputValue(id, value) {
+        const modal = this.toHtml();
+        const input = modal.querySelector(`#${id}`);
+        if (input) {
+            input.value = value;
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+            input.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+        return this;
+    }
+
+    clickSave() {
+		return this.toHtml().querySelector('#submit-spending').onclick();
+	}
 }
--- test/spending/spendingSubmitModal.test.js
+++ test/spending/spendingSubmitModal.test.js
@@ -4,6 +4,7 @@
 import { describe, it, expect, beforeAll, jest } from "@jest/globals";
 import SpendingsBuilder from "./builders";
 import SpendingSubmitModalFake from "./fakes/spendingSubmitModalFake.js";
+import SpendingCategoryModal from "../../static/js/spending/view/spendingCategoryModal.js";
 
 describe('Spending Submit Modal', () => {
     beforeAll(() => {
@@ -18,7 +19,6 @@ describe('Spending Submit Modal', () => {
     });
 
 
-
     it('Should display spending correctly in edit mode', () => {
         const builder = new SpendingsBuilder();
         const spendings = builder.oneSpending().create();
@@ -30,5 +30,76 @@ describe('Spending Submit Modal', () => {
         expect(modal.readInputValue('category')).toBe('Food');
         expect(modal.readInputValue('description')).toBe('Bread');
         expect(modal.readInputValue('price')).toBe('10');
+    });
+
+    it('Should display empty fields in insert mode', () => {
+        const builder = new SpendingsBuilder();
+        const categories = builder.categories();
+        const modal = new SpendingSubmitModalFake(categories, 2001, 1, 1)
+            .insertMode();
+
+        expect(modal.readInputValue('date')).toBe('2001-02-01');
+        expect(modal.readInputValue('category')).toBe('');
+        expect(modal.readInputValue('description')).toBe('');
+        expect(modal.readInputValue('price')).toBe('');
+    });
+
+    it('Should not call onSubmit callback when form is invalid', () => {
+        const builder = new SpendingsBuilder();
+        const categories = builder.categories();
+        const onSubmit = jest.fn();
+        new SpendingSubmitModalFake(categories, 2001, 1, 1)
+            .insertMode()
+            .onSubmit(onSubmit)
+            .open()
+            .clickSave();
+
+        expect(onSubmit).not.toHaveBeenCalled();
+    });
+
+    it('Should call onSubmit callback when form is valid', () => {
+        const builder = new SpendingsBuilder();
+        const categories = builder.categories();
+        const modal = new SpendingSubmitModalFake(categories, 2001, 1, 1)
+            .insertMode()
+            .onSubmit((expectedSpending) => {
+                expect(expectedSpending.spentOn).toEqual(new Date('2001-02-01'));
+                expect(expectedSpending.category).toEqual('Food');
+                expect(expectedSpending.description).toEqual('Bread');
+                expect(expectedSpending.price).toEqual(10);
+            })
+            .open()
+            .setInputValue('date', '2001-02-01')
+            .setInputValue('category', 'Food')
+            .setInputValue('description', 'Bread')
+            .setInputValue('price', '10')
+            .clickSave();
+    });
+
+    it('Should open category modal when clicking category input', () => {
+        const builder = new SpendingsBuilder();
+        const categories = builder.categories();
+        
+        const modal = new SpendingSubmitModalFake(categories, 2001, 1, 1)
+            .insertMode()
+            .openCategoryModal();
+        
+        expect(modal instanceof SpendingCategoryModal).toBe(true);
+    });
+
+    it('Should update category input when selecting a category', () => {
+        const builder = new SpendingsBuilder();
+        const categories = builder.categories();
+        const spendingModal = new SpendingSubmitModalFake(categories, 2001, 1, 1)
+            .insertMode()
+            .open();
+        
+        spendingModal
+            .openCategoryModal()
+            .toHtml()
+            .querySelector('.accordion-secondary')
+            .onclick({ target: { textContent: 'Food' } });
+
+        expect(spendingModal.readInputValue('category')).toBe('Food');
     });
 })
